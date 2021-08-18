b8efd2e3fcac72333078e62d771718b14e773f98
TomekoLeko <ays@vp.pl>
2021-08-18 13:28:30 +0200

    zainstalownay bootstrap

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
 
 Vue.config.productionTip = false
 
@@ -10,3 +17,7 @@ new Vue({
 }).$mount('#app')
 
 Vue.use(VueAxios, axios)
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
